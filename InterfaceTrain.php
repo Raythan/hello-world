25b284ca63dd614d22b22c63f7857c2d0c8cd481
Raythan <33856352+Raythan@users.noreply.github.com>
2018-08-08 13:28:04 -0300

    InterfaceTrain.php
    Código de interface em PHP onde um dos comandos é passado por parâmetros

---
+++ InterfaceTrain.php
@@ -0,0 +1,10 @@
+<?php
+interface InterfaceTrain {
+    public function pegarAtributo1();
+    public function colocarAtributo1();
+    public function pegarAtributo2();
+    public function colocarAtributo2($p); // Código da interface passando parâmetros
+    public function pegarAtributo3();
+    public function colocarAtributo3();
+}
+// Basicamente os métodos da interface tem abstract como padrão por isso devem ser omitidos quando digitados no NetBeans
