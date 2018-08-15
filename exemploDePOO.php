6a748082ccba1015c597f7e3d29c986a110040d3
Raythan <33856352+Raythan@users.noreply.github.com>
2018-08-15 19:10:45 -0300

    Update exemploDePOO.php

--- exemploDePOO.php
+++ exemploDePOO.php
@@ -21,6 +21,17 @@
             $teste->colocarAtributo3();
             $teste->pegarAtributo3(); 
         ?>
+        
+        <?php
+            interface InterfaceTrain {
+                public function pegarAtributo1();
+                public function colocarAtributo1();
+                public function pegarAtributo2();
+                public function colocarAtributo2($p); // Código da interface passando parâmetros
+                public function pegarAtributo3();
+                public function colocarAtributo3();
+            }
+        ?>
         </pre>
     </body>
 </html>
